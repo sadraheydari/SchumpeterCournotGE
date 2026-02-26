f8a51ac1128cabd4c53432f4601d6e19dd73cb7e
sadraheydari <msadrah521@Gmail.com>
2026-02-26 14:12:02 +0000

    create value_scaling_model

---
+++ src/core/ScaleMode.jl
@@ -0,0 +1,24 @@
+
+# ============================================================
+# Value Scaling Modes
+# ============================================================
+
+abstract type ValueScaling end
+struct Levels <: ValueScaling end
+struct Detrended <: ValueScaling end
+
+
+# ============================================================
+# Flow Scaling
+# ============================================================
+
+scale_flow(::Levels, flow, A_i) = flow
+scale_flow(::Detrended, flow, A_i) = flow / A_i
+
+
+# ============================================================
+# Kernel Scaling
+# ============================================================
+
+scale_kernel(::Levels, sdf, A_old, A_new) = sdf
+scale_kernel(::Detrended, sdf, A_old, A_new) = sdf * (A_new / A_old)
