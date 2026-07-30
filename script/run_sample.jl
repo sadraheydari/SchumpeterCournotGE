ec592b626a288a9eff3046fab37fc10a6b896a83
sadraheydari <msadrah521@Gmail.com>
2026-07-30 12:05:12 +0100

    update run_sample.jl: modify model parameters and add result plotting

--- script/run_sample.jl
+++ script/run_sample.jl
@@ -4,13 +4,14 @@ using .SchumpeterCournotGE
 res = run_model(
     n=3,
     β = 0.96, σ = 2.0, μ = 2.0, γ = 1.06,
-    θ = 0.10, ε = 1.50, η̄ = 1.5,
-    amin = 0.05, amax = 8.0, k = 50,
+    θ = 0.20, ε = 1.50, η̄ = 1.5,
+    amin = 0.05, amax = 6.0, k = 40,
     spacing = :log, spacing_param = 2.0,
-    g_w = 0.021526, g_y = 0.021526, ŷ = 1.334950,
+    g_w = 0.029799, g_y = 0.029799, ŷ = 1.243426,
     tol_vfi = 1e-10, maxiter_vfi = 8_000,
     tol_sym = 1e-7, maxiter_sym = 150, λ_sym = 0.25,
     tol_agg = 1e-5, maxiter_agg = 40, λ_agg = 0.4,
     n_sims = 2_000, n_periods = 500, burnin = 100,
     seed = 20260730
 )
+plot_results(res; save = "output/run_sample.png")
