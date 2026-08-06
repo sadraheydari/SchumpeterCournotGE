328b396f5e549bbe9abc874a7418842a21424dc7
sadraheydari <msadrah521@Gmail.com>
2026-08-06 08:45:01 +0100

    update run_sample script with new model parameters and simulation settings

--- script/run_sample.jl
+++ script/run_sample.jl
@@ -2,21 +2,32 @@ include("../src/SchumpeterCournotGE.jl")
 using .SchumpeterCournotGE
 
 res = run_model(
-    n=4,
-    β = 0.978, σ = 3.34, μ = 3.87, γ = 1.08,
-    θ = 0.25, 
-    ε = 2.76, 
-    # ε = 0.75,
-    η̄ = 1.35,
-    amin = 0.001, amax = 12.5, k = 50, ky=10,
-    spacing = :log, spacing_param = 2.0,
-    g_w = 0.024902, g_y = 0.024902, ŷ = 1.139090, # n=3 res
-    # g_w = 0.029027, g_y = 0.029027, ŷ = 1.050511, # n=4 res
-    tol_vfi = 1e-9, maxiter_vfi = 1_000,
-    tol_sym = 1e-7, maxiter_sym = 150, λ_sym = 0.1,
-    tol_agg = 1e-5, maxiter_agg = 100, λ_agg = 0.1,
-    n_sims = 1_000, n_periods = 500, burnin = 100,
-    seed = 20260910
-)
+           n=3,
+           β = 0.97, σ = 1.5, μ = 3.5, γ = 1.05,
+           θ = 0.1, ε = 1.5, η̄ = 0.7,
+           amin = 0.05, amax = 5.0, k = 100,
+           spacing = :log, spacing_param = 10.0,
+           g_w = 0.01494, 
+           g_y = 0.01494, 
+           ŷ = 1.2012,
+           tol_vfi = 1e-9, maxiter_vfi = 1_000,
+           tol_sym = 1e-7, maxiter_sym = 150, λ_sym = 0.3,
+           tol_agg = 1e-5, maxiter_agg = 100, λ_agg = 0.2,
+           n_sims = 5_000, n_periods = 1_300, burnin = 300,
+           seed = 20260910
+       )
 
-plot_results(res; save = "output/run_sample_n4.png")
+save_config("output/n3-triopoly/models/callibrated.toml", res.model)
+save_model("output/n3-triopoly/models/callibrated.jld2", res.model)
+
+set = SimSettings(n_industries = 5_000,
+                  n_periods    = 1_300,
+                  burnin       = 300,
+                  thin         = 1,
+                  seed         = 20260801)
+
+sim = run_simulation(res.model, set)
+sim_report(sim)
+sim_report(sim;        save = "output/n3-triopoly/sim")
+plot_stationarity(sim; save = "output/n3-triopoly/sim")
+plot_industry(sim;     save = "output/n3-triopoly/sim")
